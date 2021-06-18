ff27f2f4e742e4e7083e87aa3ca49ede92a40513
Sagar-Sathyanarayanan <sagar.sathyanarayan@codemancers.com>
2021-06-18 16:56:22 +0530

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ array=(${string//,/ })
 
 
 for i in "${!array[@]}"; do
-  echo "Deploy on EC2"
+  echo "Deploy on EC2 AWS"
   echo "Deploy django app on to the server ${array[i]}"
   ssh ubuntu@${array[i]}  "cd ecomm-live && git pull origin main && sudo docker-compose build && docker-compose up"
 done
