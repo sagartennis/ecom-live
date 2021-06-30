b266cb1bb49db124b4a9dcf52e928e2c256b4835
sagartennis <sagarthetennis10k@gmail.com>
2021-06-30 19:33:18 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = 'Eshop.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+    default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
